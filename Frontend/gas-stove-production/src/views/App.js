27e0fedd08cd6ccd654c752d98e51a3594c572c3
trungnd-fpt-se140274 <55337563+trungnd-fptu-se140274@users.noreply.github.com>
2022-02-22 00:11:40 +0700

    [GSP] Update source code front end

---
+++ Frontend/gas-stove-production/src/views/App.js
@@ -0,0 +1,13 @@
+import AdminDashboard from './login/AdminDashboard';
+import CreateAccount from './admin/CreateAccount';
+
+function App() {
+  return (
+    <>
+      {/* <AdminDashboard /> */}
+      <CreateAccount />
+    </>
+  );
+}
+
+export default App;
