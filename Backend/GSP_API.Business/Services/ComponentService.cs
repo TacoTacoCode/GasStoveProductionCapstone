b506df6ba49800891ffdc25940bb6243023e8e31
thinhlhsedev <thinhlhse140448@fpt.edu.vn>
2022-03-12 16:01:41 +0700

    [GSP] Fix bug

--- Backend/GSP_API.Business/Services/ComponentService.cs
+++ Backend/GSP_API.Business/Services/ComponentService.cs
@@ -30,9 +30,25 @@ namespace GSP_API.Business.Services
             return await _componentRepository.GetById(p => p.ComponentId == componentId);
         }
 
-        public async Task<string> AddComponent(Component component)
+        public async Task<string> AddComponent(Component component, List<Material> materialChosen)
         {
-            return await _componentRepository.Add(component);
+            var compoMateService = new ComponentMaterialService();
+
+            var data = await _componentRepository.Add(component);
+            var listCompoMate = new List<ComponentMaterial>();
+            switch (data)
+            {
+                case "true":
+                    //For each compo in chosen list, create a correspoding ProCompo  
+                    foreach (Material material in materialChosen)
+                    {
+                        ComponentMaterial compoMate = new ComponentMaterial(component.ComponentId, material.MaterialId, component.Amount);
+                        listCompoMate.Add(compoMate);
+                    }
+                    return await compoMateService.AddRangeCompoMate(listCompoMate);
+                default:
+                    return data;
+            }
         }
 
         public async Task<string> UpdateComponent(Component newComponent)
